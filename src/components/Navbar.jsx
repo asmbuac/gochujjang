01fc67582f8ff172f10d212561f952a079903357
Shayne <asmbuac@gmail.com>
2023-06-30 21:56:55 -0700

    Created announcement component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import Badge from '@mui/material/Badge';
 import { Search, ShoppingCartOutlined } from '@mui/icons-material';
-import React from 'react';
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 const Container = styled.div`
   height: 60px;
