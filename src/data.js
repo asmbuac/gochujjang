1a5eeedf829e69ff4bd0f49f6b3952128e608ad2
Shayne <asmbuac@gmail.com>
2023-06-30 22:57:58 -0700

    Created hero slider for home page

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.pngall.com/wp-content/uploads/10/Blackpink-PNG-HD-Image.png",
+    title: "SUMMER SALE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "F5FAFD",
+  },
+  {
+    id: 2,
+    img: "https://www.pngall.com/wp-content/uploads/10/Blackpink-PNG-HD-Image.png",
+    title: "FALL COLLECTION",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "FCF1ED",
+  },
+  {
+    id: 3,
+    img: "https://www.pngall.com/wp-content/uploads/10/Blackpink-PNG-HD-Image.png",
+    title: "POPULAR ITEMS",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "FBF0F4",
+  },
+];
