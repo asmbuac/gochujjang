74911aff956b3ae79f573f6d5865bdae2bf6dd8f
Shayne <asmbuac@gmail.com>
2023-07-03 19:35:54 -0700

    Created login route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,7 +21,29 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
+});
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("User does not exist");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET
+    );
+    const ogPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    ogPassword !== req.body.password &&
+      res.status(400).json("Incorrect password");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
