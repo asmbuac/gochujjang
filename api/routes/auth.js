be34550771bae79f4b39f5f6029394bf40a85d7e
Shayne <asmbuac@gmail.com>
2023-07-03 19:19:56 -0700

    Installed CryptoJS and encrypted password in register function

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // SIGNUP
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString(),
     firstName: req.body.firstName,
     lastName: req.body.lastName,
   });
