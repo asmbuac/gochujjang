3dcfacfb99de7907d72430ec33fe8e048dfd4cf3
Shayne <asmbuac@gmail.com>
2023-07-03 18:49:11 -0700

    Created Order model and deleted test routes in user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,3 @@
 const router = require("express").Router();
 
-router.get("/test", (req, res) => {
-  res.send("User test is successful");
-});
-
-router.post("/posttest", (req, res) => {
-  const username = req.body.username;
-  res.send(`Your username is ${username}`);
-});
-
 module.exports = router;
