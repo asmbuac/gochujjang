c24d34941bfea9970e8dbab1deeb726c4b3846ec
Shayne <asmbuac@gmail.com>
2023-07-10 18:04:38 -0700

    Added routes for product category and product detail pages. Also added redirects to home page if already logged in if trying to access login or register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,12 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import Pay from "./pages/Pay";
 import Success from "./pages/Success";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Newsletter from "./components/Newsletter";
 import Footer from "./components/Footer";
 
 const App = () => {
+  const user = true;
 
   return (
     <BrowserRouter>
@@ -22,9 +23,13 @@ const App = () => {
       <Marquee />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<ProductList />} />
+        <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
+        <Route path="/products">
+          <Route index element={<ProductList />} />
+          <Route path=":category" element={<ProductList />} />
+        </Route>
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/pay" element={<Pay />} />
         <Route path="/success" element={<Success />} />
