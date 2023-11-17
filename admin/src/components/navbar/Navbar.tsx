ab24563161feeab1e52ff24da7d7236cbca25696
Shayne <asmbuac@gmail.com>
2023-11-16 22:00:20 -0800

    Added icons and styling to navbar. Finished design but no functionality for now

--- admin/src/components/navbar/Navbar.tsx
+++ admin/src/components/navbar/Navbar.tsx
@@ -1,7 +1,38 @@
+import {
+  CropFreeSharp,
+  GridView,
+  NotificationsOutlined,
+  Search,
+  SettingsOutlined,
+} from "@mui/icons-material";
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <img src="src/assets/logo.png" alt="" />
+        <span>GochuJjang</span>
+      </div>
+      <div className="right">
+        <Search className="icon" />
+        <GridView className="icon" />
+        <CropFreeSharp className="icon" />
+        <div className="notifications">
+          <NotificationsOutlined className="icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://media.licdn.com/dms/image/C4E03AQHEHCSHtBJMQQ/profile-displayphoto-shrink_800_800/0/1661277175020?e=1705536000&v=beta&t=xCeST-m10o-habV9cJXpARmIeSj_wTNGix6Qqy8zMu4"
+            alt=""
+          />
+          <span>Shayne</span>
+        </div>
+        <SettingsOutlined className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
