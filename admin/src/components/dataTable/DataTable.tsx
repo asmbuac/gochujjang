5874b8b13fead1af29d5c223644cd91ec9385e12
Shayne <asmbuac@gmail.com>
2023-11-20 17:50:49 -0800

    Added mock data for users, products, and single user page (will integrate server side data later on). Users data table mostly done

--- admin/src/components/dataTable/DataTable.tsx
+++ admin/src/components/dataTable/DataTable.tsx
@@ -1,7 +1,40 @@
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
 
-const DataTable = () => {
-  return <div className="dataTable"></div>;
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+};
+
+const DataTable: React.FC<Props> = ({ columns, rows }) => {
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableColumnSelector
+      />
+    </div>
+  );
 };
 
 export default DataTable;
