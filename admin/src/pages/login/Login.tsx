8b7f230602585ade10d3da11e630d3ccc6e58676
Shayne <asmbuac@gmail.com>
2023-11-23 02:57:39 -0800

    Finished styling login page

--- admin/src/pages/login/Login.tsx
+++ admin/src/pages/login/Login.tsx
@@ -1,5 +1,19 @@
+import "./login.scss";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <img src="/src/assets/logo.png" alt="" />
+      <div className="wrapper">
+        <h1>Login</h1>
+        <form>
+          <input type="text" required placeholder="Username or Email" />
+          <input type="password" required placeholder="Password" />
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
