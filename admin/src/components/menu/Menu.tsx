d531d9db60c3e8580173ff7124338b42d6411261
Shayne <asmbuac@gmail.com>
2023-11-24 23:28:55 -0800

    Incorporated logout in menu with logout functionality. Successfully redirects to login page on logout

--- admin/src/components/menu/Menu.tsx
+++ admin/src/components/menu/Menu.tsx
@@ -1,25 +1,41 @@
 import { Link } from "react-router-dom";
 import "./menu.scss";
 import { menu } from "../../data";
+import { Logout } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/authSlice";
 
 const Menu = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
   return (
     <div className="menu">
-      <div className="brand">
-        <img src="/src/assets/logo.png" alt="" />
-        <span>GochuJjang</span>
-      </div>
-      {menu.map((item) => (
-        <div className="item" key={item.id}>
-          <span className="title">{item.title}</span>
-          {item.listItems.map((listItem) => (
-            <Link to={listItem.url} className="listItem" key={listItem.id}>
-              <listItem.icon />
-              <span className="listItemTitle">{listItem.title}</span>
-            </Link>
-          ))}
+      <div className="top">
+        <div className="brand">
+          <img src="/src/assets/logo.png" alt="" />
+          <span>GochuJjang</span>
         </div>
-      ))}
+        {menu.map((item) => (
+          <div className="item" key={item.id}>
+            <span className="title">{item.title}</span>
+            {item.listItems.map((listItem) => (
+              <Link to={listItem.url} className="listItem" key={listItem.id}>
+                <listItem.icon />
+                <span className="listItemTitle">{listItem.title}</span>
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="logout" onClick={handleClick}>
+        <Logout />
+        <span>Logout</span>
+      </div>
     </div>
   );
 };
