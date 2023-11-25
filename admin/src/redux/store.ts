dfbc46bae1e82dda1763306158dc2add26333dfd
Shayne <asmbuac@gmail.com>
2023-11-24 22:16:04 -0800

    Installed redux, configured store, and created auth slice with login and logout reducers (need to implement later in login page)

---
+++ admin/src/redux/store.ts
@@ -0,0 +1,33 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
