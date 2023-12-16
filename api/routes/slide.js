42cd36bff42bee91ff5942649f50f81101a5fc0b
Shayne <asmbuac@gmail.com>
2023-12-15 19:57:14 -0800

    PUT and GET all endpoints for slides

--- api/routes/slide.js
+++ api/routes/slide.js
@@ -9,9 +9,35 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 
   try {
     const savedSlide = await slide.save();
-    res.status(200).json(savedSlide);
+    res.status(201).json(savedSlide);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+// UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedSlide = await Slide.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedSlide);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// GET ALL SLIDES
+router.get("/", async (req, res) => {
+  try {
+    const slides = await Slide.find();
+    res.status(200).json(slides);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
