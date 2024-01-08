3bd7e26761e06523cbf3bc199a2ce248da27fa46
Shayne <asmbuac@gmail.com>
2024-01-07 16:49:57 -0800

    Started search drawer

---
+++ client/src/components/SearchDrawer.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const SearchDrawer = () => {
+  return <Container>Search Drawer</Container>;
+};
+
+export default SearchDrawer;
